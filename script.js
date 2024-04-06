45bb397ad36bd7190b3d241164703a05a04cba14
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-04-06 20:43:24 +0530

    Photo updated

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer"],
+  strings: ["Frontend Developer", "ReactJS Developer"],
   typeSpeed: 50,
   backSpeed: 100,
   bacDelay: 1000,
